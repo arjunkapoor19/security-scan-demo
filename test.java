43c1f0527cbdbc3d385824da80623d1e8b4440c5
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 16:01:57 +0530

    Intentionall injecting insecure code

--- test.java
+++ test.java
@@ -1,7 +1,12 @@
-public class test 
-    { public static void main(String[] args) 
-        { 
-            System.out.println("Hello World Part 2"); 
-            System.out.println("SEMGrep test");
-        } 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class VulnerableSQL {
+    public static void main(String[] args) throws Exception {
+        String userInput = args[0];
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "root");
+        Statement stmt = conn.createStatement();
+        stmt.execute("SELECT * FROM users WHERE name = '" + userInput + "'");
     }
+}
