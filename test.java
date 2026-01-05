3850ebd22e3da4a81546e013ded19376ac1d5619
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 16:22:21 +0530

    Removing the injection

--- test.java
+++ test.java
@@ -4,9 +4,6 @@ import java.sql.Statement;
 
 public class VulnerableSQL {
     public static void main(String[] args) throws Exception {
-        String userInput = args[0];
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "root");
-        Statement stmt = conn.createStatement();
-        stmt.execute("SELECT * FROM users WHERE name = '" + userInput + "'");
+        System.out.println("Starting SQL Injection Example");
     }
 }
