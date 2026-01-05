097aa720aecbbcd5ac809a6cc7ef10924d45a7ce
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 18:01:01 +0530

    Removed

--- test.java
+++ test.java
@@ -3,8 +3,6 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class WeakCrypto {
     public static void main(String[] args) throws Exception {
-        Cipher cipher = Cipher.getInstance("DES");
-        SecretKeySpec key = new SecretKeySpec("12345678".getBytes(), "DES");
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        System.out.println("Using weak cryptography...");
     }
 }
