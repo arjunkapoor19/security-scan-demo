7fb80da6466dcb1b8a2a7c520ca223f264f3986f
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 16:39:08 +0530

    Injection try 3

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
-public class CommandInjection {
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class WeakCrypto {
     public static void main(String[] args) throws Exception {
-        String userInput = args[0];
-        Runtime.getRuntime().exec(userInput);
+        Cipher cipher = Cipher.getInstance("DES");
+        SecretKeySpec key = new SecretKeySpec("12345678".getBytes(), "DES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
     }
 }
