626d86cd0832ae06acf6e8411cf9095d8ac51870
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 15:58:39 +0530

    Semgrep test on test.java

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test
     { public static void main(String[] args) 
         { 
             System.out.println("Hello World Part 2"); 
+            System.out.println("SEMGrep test");
         } 
     }
