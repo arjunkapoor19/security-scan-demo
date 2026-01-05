a63f807f69ac921bd8344cac6166dd185df4fc6e
Arjun Kapoor <arjunk@LT-MBP-018.local>
2026-01-05 17:43:54 +0530

    Adding injection

--- test.java
+++ test.java
@@ -3,6 +3,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class WeakCrypto {
     public static void main(String[] args) throws Exception {
-        System.out.println("Using weak cryptography...");
+        Cipher cipher = Cipher.getInstance("DES");
+        SecretKeySpec key = new SecretKeySpec("12345678".getBytes(), "DES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
     }
 }
